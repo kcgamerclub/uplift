8041d16aeaad4b0856bbd1e804cee97a07f161c7
kcgamerclub <kcgamerclub@gmail.com>
2026-07-28 19:38:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 function openTraining() {
-    alert("Training page coming soon! 💪🩷");
+
+    document.getElementById("home-page").style.display = "none";
+
+    document.getElementById("training-page").style.display = "block";
+
+}
+
+
+function goHome() {
+
+    document.getElementById("training-page").style.display = "none";
+
+    document.getElementById("home-page").style.display = "block";
+
 }
