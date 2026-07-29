66e7cb43e24d5cbb1e086b181e7e8bee59613c99
kcgamerclub <kcgamerclub@gmail.com>
2026-07-28 20:25:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,133 @@
-function openSchedule() {
+const today = new Date().getDay();
 
-    document.getElementById("home-page").classList.add("hidden");
 
-    document.getElementById("schedule-page").classList.remove("hidden");
+const workouts = {
+
+0: {
+day: "Sunday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Full Backspot Strength"
+]
+
+},
+
+
+1: {
+day: "Monday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Lower Body Workout",
+"Upper Body Workout"
+]
+
+},
+
+
+2: {
+day: "Tuesday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Full Backspot Strength"
+]
+
+},
+
+
+3: {
+day: "Wednesday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Lower Body Workout",
+"Strength Workout"
+]
+
+},
+
+
+4: {
+day: "Thursday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Full Backspot Strength"
+]
+
+},
+
+
+5: {
+day: "Friday",
+
+morning: [
+"Lilly Sabri Abs",
+"Deep Core Activation",
+"Jumps & Flexibility"
+],
+
+night: [
+"Lilly Sabri Glutes",
+"Upper Body Workout",
+"Strength Workout"
+]
 
 }
 
+};
 
 
-function goHome() {
 
-    document.getElementById("schedule-page").classList.add("hidden");
+const workoutToday = workouts[today];
 
-    document.getElementById("home-page").classList.remove("hidden");
 
-}
+
+document.getElementById("day-title").innerHTML =
+"🩷 " + workoutToday.day + "'s Training";
+
+
+
+document.getElementById("morning-workout").innerHTML =
+workoutToday.morning
+.map(item => "⬜ " + item)
+.join("<br><br>");
+
+
+
+document.getElementById("night-workout").innerHTML =
+workoutToday.night
+.map(item => "⬜ " + item)
+.join("<br><br>");
