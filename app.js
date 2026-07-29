2f8231fc101075e9ab14614e54c903ad688f79b6
kcgamerclub <kcgamerclub@gmail.com>
2026-07-28 20:08:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ function openTraining() {
 }
 
 
+
+function openStrength() {
+
+    document.getElementById("training-page").classList.add("hidden");
+
+    document.getElementById("strength-page").classList.remove("hidden");
+
+}
+
+
+
+function goTraining() {
+
+    document.getElementById("strength-page").classList.add("hidden");
+
+    document.getElementById("training-page").classList.remove("hidden");
+
+}
+
+
+
 function goHome() {
 
     document.getElementById("training-page").classList.add("hidden");
